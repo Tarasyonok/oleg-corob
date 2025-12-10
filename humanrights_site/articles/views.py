60245c32f449b57976057c0769aaaf26e4f97046
Kir Tarasov <kir_tarasov@mail.ru>
2025-12-10 16:34:30 +0300

    123123

--- humanrights_site/articles/views.py
+++ humanrights_site/articles/views.py
@@ -14,7 +14,7 @@ class ArticleListView(django.views.generic.ListView):
     model = Article
     template_name = "articles/list.html"
     context_object_name = "articles"
-    paginate_by = 1
+    paginate_by = 7
 
     def get_queryset(self):
         queryset = super().get_queryset()
